b039f0b720664ee028dea69181234e9efaf0ada6
recove6005 <leehan6005@gmail.com>
2025-07-01 17:29:22 +0900

    eidt code

--- suparound-py-server/wba-schedule.py
+++ suparound-py-server/wba-schedule.py
@@ -24,8 +24,9 @@ def get_wba_schedule():
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     options.add_argument('--no-sandbox')
-    options.add_argument("window-size=1920x1080")
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36")
+    options.add_argument("--disable-dev-shm-usage")
+    # options.add_argument("window-size=1920x1080")
+    # options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36")
 
     driver = webdriver.Chrome(options=options)
 
